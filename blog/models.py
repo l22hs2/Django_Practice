2ffe18d5d684278c1abbb081426059d94ebf7ae9
l22hs2 <l22hs2@gmail.com>
2022-03-31 15:26:40 +0900

    Created Post models and auto set time

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,8 @@ from django.db import models
 class Post(models.Model): # 상속
     title = models.CharField(max_length=30)
     content = models.TextField()
-    create_at = models.DateTimeField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"[{self.pk}] {self.title}"
