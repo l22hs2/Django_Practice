d3970976b2fa47f80c4a0e1140a7b93c4787e6ba
l22hs <l22hs2@gmail.com>
2022-04-30 22:55:51 +0900

    Until Write test code and Modularization

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+from distutils.command.upload import upload
+import os
 from django.db import models
 
 # Create your models here.
@@ -7,8 +9,18 @@ class Post(models.Model): # 상속
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/files/%Y/%m%d/', blank=True)
+    hook_text = models.CharField(max_length=100, blank=True)
+
     def __str__(self):
         return f"[{self.pk}] {self.title}"
     
     def get_absolute_url(self):
-        return f"/blog/{self.pk}"
+        return f"/blog/{self.pk}/"
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
